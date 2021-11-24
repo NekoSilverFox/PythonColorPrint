423b58fd33268f83cad4daeca5c5c197c8ec2d35
冰糖雪狸 <46406730+NekoSilverFox@users.noreply.github.com>
2021-11-24 12:33:53 +0300

    增加 打包文件

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/11/24 12:30
+# @Author  : Meng Jianing
+# @FileName: setup.py
+# @Software: PyCharm
+# @Versions: v0.1
+# @Github  ：https://github.com/NekoSilverFox
+# --------------------------------------------
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+VERSION = '0.1'
+
+setup(
+    name='Pycolor',
+    version=VERSION,
+    description='得到或输出带有色彩的字符串',
+    packages=find_packages(),
+    zip_safe=False
+)
